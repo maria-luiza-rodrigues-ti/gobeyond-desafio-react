4b5988923abb164db7c0f8c56cde5a4df83eb781
Maria Luiza <maria.luiza_98@hotmail.com>
2021-09-18 01:02:43 -0300

    header

---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <>
+            <div className="header-container">
+                <div className="profile-container">
+                    <img src='' alt='Foto de Perfil'/>
+                    <h1>Maria Luiza Rodrigues</h1>
+                    <h4>Front-end developer</h4>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
